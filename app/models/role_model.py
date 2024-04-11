68cb7c963f3ee9d62e2c3392fceb4108fe32e885
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2024-04-11 15:49:00 -0600

    add #5: user and roles relationship

--- app/models/role_model.py
+++ app/models/role_model.py
@@ -1,6 +1,6 @@
 """Role Model defines the database model structure for role module."""
 from sqlalchemy import Column, Integer, String, text
-
+from sqlalchemy.orm import relationship
 from app.config.database.base_class import Base
 from app.utils.datetime_custom_type import CustomDateTime
 
@@ -13,6 +13,7 @@ class Role(Base):
                         nullable=False,
                         server_default=text("CURRENT_TIMESTAMP"))
 
+    user_role = relationship("UserRoles", back_populates="role")    
     def __repr__(self) -> str:
         return (f'<{self.id},'
                 f'{self.name}'
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,6 +1,6 @@
 """User Model defines the database model structure for user module."""
-from sqlalchemy import Column, String, Integer, text
-# from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, text, ForeignKey
+from sqlalchemy.orm import relationship
 from app.config.database.base_class import Base
 from app.utils.datetime_custom_type import CustomDateTime
 
@@ -10,7 +10,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     password = Column(String, nullable=False)
     email = Column(String, nullable=False)
-    role_id = Column(Integer, nullable=True)
+    role_id = Column(Integer, ForeignKey("role.id"), nullable=True)
     created_at = Column(CustomDateTime,
                         nullable=False,
                         server_default=text("CURRENT_TIMESTAMP"))
@@ -18,6 +18,7 @@ class User(Base):
                         nullable=False,
                         server_default=text("CURRENT_TIMESTAMP"))
 
+    user_role = relationship("UserRoles", back_populates="user")
     def __repr__(self) -> str:
         return (f'<{self.id},'
                 f'{self.email}'
